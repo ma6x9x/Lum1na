c6fe8fadb4b653b5904faaa949b33c8f0dad5be0
maxX <kolbymaxx0@gmail.com>
2026-09-20 18:43:59 -0400

    Add files via upload

---
+++ Lum1naTests/Lum1naTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lum1naTests.swift
+//  Lum1naTests
+//
+//  Created by Kolby Kehler on 9/18/26.
+//
+
+import Testing
+@testable import Lum1na
+
+struct Lum1naTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
