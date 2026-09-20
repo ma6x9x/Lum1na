daee5c11808e1a99e26ced29f6e076a13b086586
maxX <kolbymaxx0@gmail.com>
2026-09-20 19:14:56 -0400

    Fix Lum1na compile: exclude native .m, Swift stubs, align manager, unsigned CI

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -2,22 +2,9 @@
 #define BRIDGING_HEADER_H
 
 #import <Foundation/Foundation.h>
-#import <stdint.h>
-#import "KASLRLeak.h"
-#import "UPLLeak.h"
 
-// Main KRW API
-int cs_init(void);
-int cs_run(uint64_t *kb, uint64_t *ks);
-void cs_cleanup(void);
-
-// Status
-int krw_get_status(void);
-const char* krw_get_phase(void);
-
-// Combined primitives
-int necp_trigger_uaf(void);
-int ane_trigger_overflow(const char* model_path);
-int iogpu_setup_uaf(void);
+// Native Exploit/*.m (KASLRLeak, UPLLeak, ClearSword, Lum1naKRW) are excluded from
+// the Lum1na app target via membershipExceptions. UI builds use Swift stubs in
+// Exploit/Bridges/NativeLeakStubs.swift. Do not import IOKit-backed headers here.
 
 #endif
