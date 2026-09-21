c1869b67f015c7d80a821ec08f78e431fc91fe7b
maxX <kolbymaxx0@gmail.com>
2026-09-21 15:31:23 -0400

    Add FusionChain and FusionChainDelegate imports

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -4,11 +4,24 @@
 #import <Foundation/Foundation.h>
 #include <stdint.h>
 
+// Base exploit primitives
 #import "Exploit/KASLRLeak.h"
 #import "Exploit/UPLLeak.h"
 #import "Exploit/CSKRW.h"
 #import "Exploit/ANE.h"
+
+// Fusion Chain (REQUIRED - used by ExploitManager.swift)
 #import "Exploit/Lum1naKRW.h"
+#import "Exploit/FusionChain.h"
+#import "Exploit/Bridges/FusionChainDelegate.h"
+
+// CVE Controllers (optional - only if Swift needs direct access)
+// These are used internally by FusionChain.m, so not strictly required here
+// #import "Exploit/P009Controller.h"
+// #import "Exploit/P052Controller.h"
+// #import "Exploit/P039Controller.h"
+
+// Bridges
 #import "Exploit/Bridges/cs_run.h"
 
 #endif
