cf3e587e5d0d7f085940594736bea0025f110f21
maxX <kolbymaxx0@gmail.com>
2026-09-21 15:54:03 -0400

    Remove comments and clean up bridging header

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -1,11 +1,10 @@
-// Lum1na-Bridging-Header.h
 #ifndef Lum1na_Bridging_Header_h
 #define Lum1na_Bridging_Header_h
 
 #import <Foundation/Foundation.h>
 #include <stdint.h>
 
-// Exploit headers - these are in Exploit/ folder
+// These work from root level with Exploit/ prefix
 #import "Exploit/KASLRLeak.h"
 #import "Exploit/UPLLeak.h"
 #import "Exploit/CSKRW.h"
@@ -13,8 +12,6 @@
 #import "Exploit/Lum1naKRW.h"
 #import "Exploit/FusionChain.h"
 #import "Exploit/Bridges/FusionChainDelegate.h"
-
-// Bridges - cs_run.h handles its own imports
 #import "Exploit/Bridges/cs_run.h"
 
 #endif
