fe90e28c039357232d086c74b455b5835e280adb
maxX <kolbymaxx0@gmail.com>
2026-09-21 10:27:27 -0400

    Refactor imports in Lum1na-Bridging-Header.h

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -4,15 +4,11 @@
 #import <Foundation/Foundation.h>
 #include <stdint.h>
 
-// Exploit primitives
 #import "KASLRLeak.h"
 #import "UPLLeak.h"
 #import "CSKRW.h"
 #import "ANE.h"
 
-// ANE C API (from ANE.m)
-int cs_init(void);
-int cs_run(uint64_t *kb, uint64_t *ks);
-void cs_cleanup(void);
+#import "cs_run.h"
 
-#endif /* Lum1na_Bridging_Header_h */
+#endif
