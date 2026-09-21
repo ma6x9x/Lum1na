b40aa05af26ae06ae99162633a8aca4b50649e36
maxX <kolbymaxx0@gmail.com>
2026-09-21 15:47:57 -0400

    Clean up Lum1na-Bridging-Header.h
    Removed commented-out CVE Controllers and unnecessary comments.

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -1,27 +1,20 @@
+// Lum1na-Bridging-Header.h
 #ifndef Lum1na_Bridging_Header_h
 #define Lum1na_Bridging_Header_h
 
 #import <Foundation/Foundation.h>
 #include <stdint.h>
 
-// Base exploit primitives
+// Exploit headers - these are in Exploit/ folder
 #import "Exploit/KASLRLeak.h"
 #import "Exploit/UPLLeak.h"
 #import "Exploit/CSKRW.h"
 #import "Exploit/ANE.h"
-
-// Fusion Chain (REQUIRED - used by ExploitManager.swift)
 #import "Exploit/Lum1naKRW.h"
 #import "Exploit/FusionChain.h"
 #import "Exploit/Bridges/FusionChainDelegate.h"
 
-// CVE Controllers (optional - only if Swift needs direct access)
-// These are used internally by FusionChain.m, so not strictly required here
-// #import "Exploit/P009Controller.h"
-// #import "Exploit/P052Controller.h"
-// #import "Exploit/P039Controller.h"
-
-// Bridges
+// Bridges - cs_run.h handles its own imports
 #import "Exploit/Bridges/cs_run.h"
 
 #endif
