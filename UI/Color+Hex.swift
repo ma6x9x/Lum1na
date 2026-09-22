4830e33966e47ab5644c05651c637d2c34d48afc
maxX <kolbymaxx0@gmail.com>
2026-09-21 23:58:14 -0400

    Add hex initializer to Color extension

---
+++ UI/Color+Hex.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        let scanner = Scanner(string: hex)
+        var rgb: UInt64 = 0
+        
+        guard scanner.scanHexInt64(&rgb) else {
+            self = .clear
+            return
+        }
+        
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >> 8) & 0xFF) / 255.0
+        let b = Double(rgb & 0xFF) / 255.0
+        
+        self.init(red: r, green: g, blue: b)
+    }
+}
