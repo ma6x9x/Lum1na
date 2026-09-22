95e69fdd043cb6de9646b6ac59baf237af851c94
maxX <kolbymaxx0@gmail.com>
2026-09-22 12:48:14 -0400

    Update Lum1na-Bridging-Header.h

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -2,18 +2,9 @@
 #define Lum1na_Bridging_Header_h
 
 #import <Foundation/Foundation.h>
-#include <stdint.h>
 
-// These work from root level with Exploit/ prefix
-#import "Exploit/KASLRLeak.h"
-#import "Exploit/UPLLeak.h"
-#import "Exploit/CSKRW.h"
-#import "Exploit/ANE.h"
-#import "Exploit/Lum1naKRW.h"
-#import "Exploit/FusionChain.h"
-#import "Exploit/Bridges/FusionChainDelegate.h"
-#import "Exploit/Bridges/cs_run.h"
-#import "Exploit/P009Controller.h"
-#import "Exploit/P052Controller.h"
-#import "Exploit/P039Controller.h"
+// Minimal exploit primitives only
+#import "Exploit/CVE_2026_65343_AKS.h"
+#import "Exploit/APFS84523.h"
+
 #endif
