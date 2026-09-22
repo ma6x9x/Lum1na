ac7502260f3460b5f1eed022540d2e0fcc0944de
maxX <kolbymaxx0@gmail.com>
2026-09-22 14:43:42 -0400

    Update Lum1na-Bridging-Header.h

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -1,11 +1,28 @@
 #ifndef Lum1na_Bridging_Header_h
 #define Lum1na_Bridging_Header_h
 
+// System headers first
 #import <Foundation/Foundation.h>
+#import <IOKit/IOKitLib.h>
 
-// Minimal exploit primitives only
+// Runtime offset system (defines LabOffTab struct)
+#import "LabRuntimeOffsets.h"
+
+// Device detection
+#import "LabDeviceProfile.h"
+
+// Device-specific offsets
+#import "A14_23F77_LabOffsets.h"
+#import "A12X_23G71_LabOffsets.h"
+
+// Exploit primitives
 #import "CVE_2026_65343_AKS.h"
 #import "APFS84523.h"
-#import "P005JIT.h" 
+#import "P005JIT.h"
+
+// Expose C functions to Swift
+extern const LabOffTab* LabOff(void);
+extern uint64_t LabKernSlide(void);
+extern void LabSetKernSlide(uint64_t slide);
 
 #endif
