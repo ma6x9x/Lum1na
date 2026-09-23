34d8cc7405bacd58cf6ef56e2dc02bb110ae7aec
maxX <kolbymaxx0@gmail.com>
2026-09-22 21:20:55 -0400

    Update Lum1na-Bridging-Header.h

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -1,32 +1,35 @@
+//
+//  Lum1na-Bridging-Header.h
+//  No separate protocol file needed - @objc protocols are in Swift
+//
+
 #ifndef Lum1na_Bridging_Header_h
 #define Lum1na_Bridging_Header_h
 
 #import <Foundation/Foundation.h>
-#include <stdint.h>
-#include <sys/sysctl.h>
+#import <UIKit/UIKit.h>
+#import <mach/mach.h>
+#import <mach/mach_vm.h>
+#import <mach/vm_map.h>
+#import <mach-o/dyld.h>
+#import <mach-o/loader.h>
+#import <sys/mman.h>
+#import <sys/types.h>
+#import <sys/sysctl.h>
+#import <dlfcn.h>
+#import <os/log.h>
 
-// MARK: - Device & Offsets
+// MARK: - Exploit Controllers (Your Real Code)
+#import "Exploit/KASLRLeak.h"
+#import "Exploit/P044AksKaslrReach.h"
+#import "Exploit/CVE_2026_65343_AKS.h"
+#import "Exploit/P051APFSXattr.h"
+#import "Exploit/P054APFSReapList.h"
 #import "Exploit/LabRuntimeOffsets.h"
+#import "Exploit/LabDeviceProfile.h"
+
+// MARK: - Offsets
 #import "Exploit/A14_23F77_LabOffsets.h"
 #import "Exploit/A12X_23G71_LabOffsets.h"
-
-// MARK: - Exploit Controllers
-#import "Exploit/KASLRLeak.h"
-#import "Exploit/UPLLeak.h"
-#import "Exploit/ANE254InputController.h"
-#import "Exploit/CSKRW.h"
-#import "Exploit/Lum1naKRW.h"
-
-// MARK: - Chain Management
-#import "Exploit/FusionChain.h"
-#import "Exploit/Bridges/FusionChainDelegate.h"
-#import "Exploit/Bridges/cs_run.h"
-
-
-// MARK: - Exploit Controllers
-#import "Exploit/P044ExploitController.h"
-#import "Exploit/P051ExploitController.h"
-#import "Exploit/P054ExploitController.h"
-#import "Exploit/AKSExploitController.h"
 
 #endif /* Lum1na_Bridging_Header_h */
