a3c365006b71a662faaaf3070dba42b83f9e4e57
maxX <kolbymaxx0@gmail.com>
2026-09-26 17:22:30 -0400

    Update Lum1na-Bridging-Header.h

--- Lum1na-Bridging-Header.h
+++ Lum1na-Bridging-Header.h
@@ -80,6 +80,9 @@
 #import "Exploit/ANEDirectIn.h"
 #import "Exploit/IOGPU64788Controller.h"
 #import "Exploit/P062StaleEntryOracle.h"
+#import "Exploit/P06xTaps.h"
+#import "Exploit/P064AVE84607Racer.h"
+#import "Exploit/P06xLog.h"
 
 
 #endif
